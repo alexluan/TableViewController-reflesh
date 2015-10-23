2b64714a0d37c1277403ba464c893f97b813643c
alexluan <326083325@qq.com>
2015-10-23 18:56:39 +0800

    Initial Commit

---
+++ reflesh/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  reflesh
+//
+//  Created by 栾有数 on 15/10/23.
+//  Copyright © 2015年 栾有数. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
